961619da13e5c007f5fc0a072a4e511b93233f63
Gabe Hargrave <ghargrave@workwithopal.com>
2017-11-14 09:44:51 -0800

    Updated

--- grade-school/grade-school.js
+++ grade-school/grade-school.js
@@ -1,11 +1,26 @@
 'use strict'
 
 class School {
+  constructor () {
+    this._roster = {}
+  }
+
+  add (name, grade) {
+    const previousGrade = this._roster[grade] || []
+    const newGrade = [...previousGrade, name].sort()
+    this._roster = Object.assign({}, 
+      this._roster, 
+      { [grade]: newGrade }
+    )
+  }
+
   roster () {
-    return {}
+    return Object.assign({}, this._roster)
   }
 
   grade (num) {
-    return []
+    return Object.assign([], this._roster[num] || [])
   }
 }
+
+module.exports = School

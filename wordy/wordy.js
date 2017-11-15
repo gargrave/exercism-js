e268ca032ac8fe0580333976c82c09509d629b3b
Gabe Hargrave <ghargrave@workwithopal.com>
2017-11-15 11:42:50 -0800

    Wordy complete

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -1,14 +1,47 @@
 'use strict'
 
+const Parser = (function () {
+  const ops = {
+    'plus': (a, b) => a + b,
+    'minus': (a, b) => a - b,
+    'multiplied': (a, b) => a * b,
+    'divided': (a, b) => a / b,
+  }
+
+  function process([a, b, ...rest], [op, ...restOps], total) {
+    if (b) {
+      return process([b, ...rest], restOps, ops[op](total || Number(a), Number(b)))
+    }
+    return total
+  }
+
+  function parse (question) {
+    const numbers = question.match(/-?\d+/g)
+    const operators = question.match(/plus|minus|multiplied|divided/g)
+    if (!numbers || !operators) {
+      throw new ArgumentError()
+    }
+    return process(numbers, operators)
+  }
+
+  return {
+    parse
+  }
+})()
+
 class WordProblem {
   constructor (question) {
+    this.question = question
   }
 
   answer () {
-    return 2
+    return Parser.parse(this.question)
   }
 }
 
+class ArgumentError {}
+
 module.exports = {
   WordProblem,
+  ArgumentError,
 }

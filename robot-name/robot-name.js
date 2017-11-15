4c2fdeafac41f1dfd4c12fc2bf1422263150d816
Gabe Hargrave <ghargrave@workwithopal.com>
2017-11-15 09:23:28 -0800

    Robot Name finished

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,8 +1,52 @@
 'use strict'
 
+const Factory = (function () {
+  const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  const usedNames = {}
+  
+  function randomAlpha () {
+    return alpha[Math.floor(Math.random() * alpha.length)]
+  }
+  
+  function randomNumber () {
+    return Math.floor(Math.random() * 10)
+  }
+  
+  function generateName () {
+    let name = null
+    while (!name || name in usedNames) {
+      name = 
+        randomAlpha() +
+        randomAlpha() +
+        randomNumber() +
+        randomNumber() +
+        randomNumber()
+    }
+    return name
+  }
+  
+  function getNewName (oldName = '') {
+    if (oldName in usedNames) {
+      usedNames[oldName] = false
+    }
+    
+    const newName = generateName()
+    usedNames[newName] = true
+    return newName
+  }
+
+  return {
+    getNewName
+  }
+})()
+
 class Robot {
   constructor () {
-    this.name = 'AA000'
+    this.name = Factory.getNewName()
+  }
+
+  reset () {
+    this.name = Factory.getNewName(this.name)
   }
 }
 

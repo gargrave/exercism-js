eceec7491adb578d42301c2cb9269a2073894640
Gabe Hargrave <ghargrave@workwithopal.com>
2017-11-16 13:22:28 -0800

    Hamming complete

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -2,6 +2,15 @@
 
 class Hamming {
   compute (a, b) {
+    if (a.length !== b.length) {
+      throw new Error('DNA strands must be of equal length.')
+    }
+    const len = a.length
+    let errors = 0
+    for (let i = 0; i < len; i++) {
+      errors += a[i] === b[i] ? 0 : 1
+    }
+    return errors
   }
 }
 
